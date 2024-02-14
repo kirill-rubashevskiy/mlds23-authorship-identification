bde14f86cd43522ccdb59a1729b338ff807bfc9e
Kirill Rubashevskiy <kirill.rubashevskiy@gmail.com>
2024-02-14 21:38:03 +0200

    - update config.yaml, project structure

--- bot/models.py
+++ bot/models.py
@@ -1,49 +1,12 @@
-import pickle
-from io import BytesIO
-
-import boto3
 import numpy as np
 import pandas as pd
 
-from bot.utils import confident_predict, label2name, preprocess_text5
-
-
-def load_model(
-    service_name: str,
-    endpoint_url: str,
-    aws_access_key_id: str,
-    aws_secret_access_key: str,
-    region_name: str,
-    bucket: str,
-    models_dir: str,
-    model_name: str,
-):
-    """
-    Load model from s3 storage.
-
-    :param service_name:
-    :param endpoint_url:
-    :param aws_access_key_id:
-    :param aws_secret_access_key:
-    :param region_name:
-    :param bucket:
-    :param models_dir:
-    :param model_name:
-    :return:
-    """
-    session = boto3.session.Session()
-    s3 = session.client(
-        service_name=service_name,
-        endpoint_url=endpoint_url,
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key,
-        region_name=region_name,
-    )
-    with BytesIO() as data:
-        s3.download_fileobj(Bucket=bucket, Key=f"{models_dir}{model_name}", Fileobj=data)
-        data.seek(0)
-        model = pickle.load(data)
-    return model
+from data_preproc.src.utils import preprocess_text5
+from mlds23_authorship_identification.models.loader import load_model
+from mlds23_authorship_identification.models.postprocessing import (
+    confident_predict,
+    label2name,
+)
 
 
 class Estimator:
