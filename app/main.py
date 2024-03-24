e45482267d1349e8d83ed3414159caea21a9c5f5
Kirill Rubashevskiy <kirill.rubashevskiy@gmail.com>
2024-03-24 20:33:58 +0200

    app: - remove env vars dependency - add comments to code - improve hydra integration

--- app/main.py
+++ app/main.py
@@ -9,9 +9,10 @@ from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from omegaconf import DictConfig
 from redis import asyncio as aioredis
+from sqlalchemy import create_engine
 
 from app import models
-from app.database import engine
+from app.database import SessionLocal
 from app.dependencies import Model
 from app.routers import items, users
 
@@ -23,7 +24,7 @@ async def lifespan(app: FastAPI, cfg: DictConfig):
     logging.info("Model loaded")
 
     # initialize Redis cache
-    redis = aioredis.from_url(f"redis://{cfg.redis.host}", decode_responses=True)
+    redis = aioredis.from_url(f"redis://{cfg.cache.host}", decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
     logging.info("Redis initialized")
     yield
@@ -31,7 +32,11 @@ async def lifespan(app: FastAPI, cfg: DictConfig):
 
 @hydra.main(version_base=None, config_path="../conf", config_name="config")
 def main(cfg: DictConfig):
+    db_url = f"postgresql://{cfg.db.user}:{cfg.db.password}@{cfg.db.host}:{cfg.db.port}/{cfg.db.name}"
+    engine = create_engine(db_url)
+    SessionLocal.configure(bind=engine)
     models.Base.metadata.create_all(bind=engine)
+
     app = FastAPI(lifespan=partial(lifespan, cfg=cfg))
     app.include_router(items.router)
     app.include_router(users.router)
@@ -40,7 +45,7 @@ def main(cfg: DictConfig):
     def root():
         return {"message": "Welcome to the Authorship Identification Service"}
 
-    uvicorn.run(app, host=cfg.app.host, port=cfg.app.port)
+    uvicorn.run(app, host="0.0.0.0", port=cfg.app.port)
 
 
 if __name__ == "__main__":
