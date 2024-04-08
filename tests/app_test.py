abd6edc1f96641485afd746ae478499b7eb97d6e
Kirill Rubashevskiy <kirill.rubashevskiy@gmail.com>
2024-04-08 18:33:46 +0300

    add doctring and comments to app_test.py and bot_test.py

--- tests/app_test.py
+++ tests/app_test.py
@@ -18,6 +18,7 @@ from mlds23_authorship_identification.utils import label2name
 
 
 def override_get_db():
+    """Overrides database session with the test database session."""
     db = TestingSessionLocal()
     try:
         yield db
@@ -26,12 +27,16 @@ def override_get_db():
 
 
 def mock_load_model(*args, **kwargs):
+    """Overrides load model from S3 for the tests where model is not used."""
     pass
 
 
+# initialize Hydra and compose a config
 with initialize(version_base=None, config_path="../conf"):
     cfg = compose(config_name="config", overrides=["cache=local", "db=test"])
 
+
+# create test database session
 db_url = f"postgresql://{cfg.db.user}:{cfg.db.password}@{cfg.db.host}:{cfg.db.port}/{cfg.db.name}"
 engine = create_engine(
     db_url,
@@ -39,12 +44,20 @@ engine = create_engine(
 )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
 app = create_app(cfg)
 app.dependency_overrides[get_db] = override_get_db
 client = TestClient(app)
 
 
-class TestDependencies:
+class TestModel:
+    """
+    Tests for app.dependencies.Model class.
+
+    Attributes:
+        model: an app.dependencies.Model instance.
+        df: a pandas.Series with texts to test the model against.
+    """
 
     model = Model(cfg.app.model_name)
     df = pd.read_csv(f"{Path(__file__).parent}/test_texts.csv").squeeze("columns")
@@ -57,8 +70,9 @@ class TestDependencies:
         "return_labels, return_names, expected",
         [(True, True, (10, 2)), (True, False, (10,)), (False, True, (10,))],
     )
-    @pytest.mark.dependency(depends=["test_load_model"])
     def test_predict(self, threshold, return_labels, return_names, expected):
+        """Tests model prediction type and shape."""
+
         prediction = self.model.predict(
             self.df,
             threshold=threshold,
@@ -69,93 +83,116 @@ class TestDependencies:
         assert prediction.shape == expected
 
     def test_predict_labels(self):
+        """Tests model prediction with labels."""
+
         prediction = self.model.predict(self.df, return_labels=True, return_names=False)
         assert prediction.dtype == "int64"
         assert np.all((prediction >= -1) & (prediction <= 9))
 
     def test_predict_names(self):
+        """Tests model prediction with names."""
+
         prediction = self.model.predict(self.df, return_labels=False, return_names=True)
         assert np.all([name in label2name.values() for name in prediction])
 
 
 @patch("app.dependencies.load_model", mock_load_model)
 class TestUsers:
+    """Tests for users-related endpoints.
+
+    Attributes:
+        prefix: prefix of all users-related endpoints.
+    """
+
+    prefix = "/users"
 
     @pytest.fixture(autouse=True)
-    def test_db(self):
+    def reset_db(self):
+        """Resets the test database between tests."""
+
         Base.metadata.create_all(bind=engine)
         yield
         Base.metadata.drop_all(bind=engine)
 
     @pytest.fixture()
-    def add_faux_users(self, request):
-        for uid in [1, 2]:
-            client.post("/users/", json={"id": uid})
+    def add_faux_users(self, request) -> None:
+        """
+        Adds faux users to the test database.
+
+        Args:
+            request: if request.param == "active", will generate active faux users (making requests, rating the
+            service).
+        """
+
+        for id in [1, 2]:
+            client.post(f"{self.prefix}/", json={"id": id})
         if request.param == "active":
-            for i, requests, rating in zip([1, 2], [3, 5], [5, 4], strict=True):
+            for id, requests, rating in zip([1, 2], [3, 5], [5, 4], strict=True):
                 for _ in range(requests):
-                    client.patch(f"/users/{i}/requests")
+                    client.patch(f"{self.prefix}/{id}/requests")
                 client.patch(
-                    f"/users/{i}/{rating}",
+                    f"{self.prefix}/{id}/{rating}",
                 )
 
         return
 
     def test_root(self):
-        response = client.get("/users/")
-
+        response = client.get(f"{self.prefix}/")
         assert response.status_code == 200
         assert response.json() == {
             "message": "Welcome to the Authorship Identification Service"
         }
 
-    def test_create_user(self):
-        response = client.post("/users/", json={"id": 3})
+    @pytest.mark.parametrize("uid", [3])
+    def test_create_user(self, uid):
+        response = client.post(f"{self.prefix}/", json={"id": uid})
         data = response.json()
-
         assert response.status_code == 200
-        assert data["id"] == 3
+        assert data["id"] == uid
         assert data["requests"] == 0
         assert data["rating"] is None
 
     @pytest.mark.parametrize("add_faux_users", ["active"], indirect=["add_faux_users"])
     def test_create_registered_user(self, add_faux_users):
-        response = client.post("/users/", json={"id": 1})
+        response = client.post(f"{self.prefix}/", json={"id": 1})
         assert response.status_code == 400
         assert response.json() == {"detail": "User already registered"}
 
-    @pytest.mark.parametrize("add_faux_users", ["passive"], indirect=["add_faux_users"])
-    def test_update_user_requests(self, add_faux_users):
+    @pytest.mark.parametrize(
+        "add_faux_users, uid", [("passive", 2)], indirect=["add_faux_users"]
+    )
+    def test_update_user_requests(self, add_faux_users, uid):
         response = client.patch(
-            "/users/2/requests",
+            f"{self.prefix}/{uid}/requests",
         )
         data = response.json()
-
         assert response.status_code == 200
-        assert data["id"] == 2
+        assert data["id"] == uid
         assert data["requests"] == 1
 
     def test_update_unregistered_user_requests(self):
+
         response = client.patch(
-            "/users/1/requests",
+            f"{self.prefix}/1/requests",
         )
         assert response.status_code == 404
         assert response.json() == {"detail": "User not found"}
 
-    @pytest.mark.parametrize("add_faux_users", ["active"], indirect=["add_faux_users"])
-    def test_update_user_rating(self, add_faux_users):
+    @pytest.mark.parametrize(
+        "add_faux_users, uid, rating", [("active", 2, 5)], indirect=["add_faux_users"]
+    )
+    def test_update_user_rating(self, add_faux_users, uid, rating):
         response = client.patch(
-            "/users/2/5",
+            f"{self.prefix}/{uid}/{rating}",
         )
         data = response.json()
-
         assert response.status_code == 200
-        assert data["id"] == 2
-        assert data["rating"] == 5
+        assert data["id"] == uid
+        assert data["rating"] == rating
 
     def test_update_unregistered_user_rating(self):
         response = client.patch(
-            "/users/1/5",
+            f"{self.prefix}/1/5",
         )
         assert response.status_code == 404
         assert response.json() == {"detail": "User not found"}
@@ -167,9 +204,8 @@ class TestUsers:
     )
     def test_get_stats(self, add_faux_users, expected_requests, expected_rating):
         with TestClient(app) as client:
-            response = client.get("/users/stats")
+            response = client.get(f"{self.prefix}/stats")
             data = response.json()
-
             assert response.status_code == 200
             assert data["total_users"] == 2
             assert data["total_requests"] == expected_requests
@@ -178,15 +214,25 @@ class TestUsers:
 
 @patch("app.dependencies.load_model", mock_load_model)
 class TestItems:
+    """Tests for predictions-related endpoints.
+
+    Attributes:
+        prefix: prefix of all predictions-related endpoints.
+    """
+
+    prefix = "/items"
 
     @patch.object(
         Model, "predict", return_value=np.array([[0, "А. Пушкин"]], dtype=object)
     )
     def test_predict_text(self, mock_predict):
-        with TestClient(app) as client:
-            response = client.post("/items/predict_text", json={"text": "some text"})
-            data = response.json()
+        """Tests single text prediction."""
 
+        with TestClient(app) as client:
+            response = client.post(
+                f"{self.prefix}/predict_text", json={"text": "some text"}
+            )
+            data = response.json()
             assert response.status_code == 200
             assert data["label"] == 0
             assert data["name"] == "А. Пушкин"
@@ -195,13 +241,14 @@ class TestItems:
         Model, "predict", return_value=np.array(["А. Пушкин"] * 10, dtype=object)
     )
     def test_predict_texts(self, mock_predict):
+        """Tests multiple texts predictions."""
+
         with TestClient(app) as client:
             with open(f"{Path(__file__).parent}/test_texts.csv", "rb") as f:
                 response = client.post(
-                    "/items/predict_texts",
+                    f"{self.prefix}/predict_texts",
                     files={"file": ("test_texts.csv", f, "text/csv")},
                 )
-
                 assert response.status_code == 200
                 assert (
                     "filename=test_texts.csv" in response.headers["content-disposition"]
