c0fcd427835327da1d61345941dc2fe3cc4c3089
Kirill Rubashevskiy <kirill.rubashevskiy@gmail.com>
2024-04-08 18:36:18 +0300

    move load_model function from dependencies.py to utils.py

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,30 +1,9 @@
-from typing import Any
-
 import numpy as np
 import pandas as pd
-from dvc.api import open
 from fastapi_cache import FastAPICache
-from joblib import load
 
 from app.database import SessionLocal
-from mlds23_authorship_identification.utils import label2name
-
-
-def load_model(model_name: str) -> Any:
-    """
-    Function loads DVC-tracked ML-model from remote S3.
-
-    :param model_name: model name
-    :return: model
-    """
-    with open(
-        path=f"models/{model_name}",
-        repo="https://github.com/kirill-rubashevskiy/mlds23-authorship-identification/",
-        mode="rb",
-        remote="yandexcloudhttp",
-    ) as f:
-        model = load(f)
-    return model
+from mlds23_authorship_identification.utils import label2name, load_model
 
 
 class Model:
